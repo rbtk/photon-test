bbd481d28e54bab28adc499eb406f97fced33e10
Atanas Chervarov <atanaschervarov@gmail.com>
2014-09-07 16:10:23 +0300

    Initial commit

---
+++ Assets/Scripts/Respawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Respawner : MonoBehaviour {
+	// Update is called once per frame
+	
+	public GameObject[] respawnObjects;
+	public GameObject player;
+
+	void Update () {
+
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+		if (respawnObjects.Length > 0)
+		{
+			int index = Random.Range(0,respawnObjects.Length);
+
+			player.transform.position = respawnObjects[index].transform.position;
+		}
+	}
+}
