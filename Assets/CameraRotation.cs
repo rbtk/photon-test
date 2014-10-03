564356ddd15b8926bbf28368ec041b4c7ceac18b
Atanas Chervarov <a.chervarov@zariba.com>
2014-10-03 10:54:12 +0300

    Adding Main Menu Scene

---
+++ Assets/CameraRotation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotation : MonoBehaviour {
+
+	void Update () {
+
+		transform.RotateAround(Vector3.zero, Vector3.up, Time.deltaTime * 10);
+		
+	}
+}
---
+++ Assets/MenuManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	private Animator menuAnimator;
+	public GameObject rotator;
+	private float rotationAngle = 0;
+	
+	void Awake()
+	{
+		menuAnimator = GetComponent<Animator>();
+	}
+	
+	void Start () 
+	{
+		menuAnimator.SetTrigger("FadeInLogo");
+	}
+	
+	public void ShowRegistrationMenu()
+	{
+		menuAnimator.SetTrigger("ShowRegistrationMenu");
+	}
+	
+	public void ShowRegistrationForm()
+	{
+		Debug.Log("Show Registration Form");
+		menuAnimator.SetTrigger("ShowRegistrationForm");
+	}
+	
+	public void Login()
+	{
+		Application.LoadLevel("MainScene");
+	}
+	
+	public void Register()
+	{
+		Application.LoadLevel("MainScene");
+	}
+	
+	public void NextCharacter()
+	{
+		rotationAngle+=90;
+		
+		if(rotationAngle>360)
+			rotationAngle = 0;
+			
+		iTween.RotateTo(rotator, iTween.Hash("z", rotationAngle));
+	}
+	
+	public void PrevCharacter()
+	{
+		rotationAngle-=90;
+		
+		if(rotationAngle < 0)
+			rotationAngle = 360;
+		
+		iTween.RotateTo(rotator, iTween.Hash("z", rotationAngle));
+	}
+}
