4d349982e93ba2550acb46a5de79ba3ec10643eb
n.indjev <n.indjev@zariba.com>
2014-10-03 17:00:03 +0300

    Pull

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	public GameObject LogoGroup;
+	public GameObject MainMenuGroup;
+	public GameObject LoginAndSignUpGroup;
+	
+	public Camera HUDCamera;
+	
+	void Start()
+	{
+//		LoginAndSignUpGroup.transform.RotateAround(HUDCamera.transform.position, Vector3.up, 180);
+	}
+	
+	public void ShowPlayMenu()
+	{
+		
+	}
+	
+	void Update()
+	{
+//		LoginAndSignUpGroup.transform.RotateAround(HUDCamera.transform.position, Vector3.up, Time.deltaTime*5);	
+	}
+}
